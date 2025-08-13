d9b5293cb1d3f67bedf5a3e44c016b6e1a022e3b
zhang Mr <185884274@qq.com>
2025-08-14 00:24:24 +0800

    拓扑带边界

--- Chap06/6_7_01PolyDataDelaunay2DWithBoundary/main.cpp
+++ Chap06/6_7_01PolyDataDelaunay2DWithBoundary/main.cpp
@@ -11,27 +11,13 @@
 #include <vtkRenderWindowInteractor.h>
 #include <vtkDelaunay2D.h>
 #include <vtkMath.h>
+#include <vtkPolygon.h>
 
 int main(int argc, char *argv[])
 {
     // Setup points
     vtkSmartPointer<vtkPoints> points =
         vtkSmartPointer<vtkPoints>::New();
-    // points->InsertNextPoint(0.0, 0.0, 0.0);    // 左下角
-    // points->InsertNextPoint(3.0, 0.0, 0.0);    // 右下角
-    // points->InsertNextPoint(3.0, 1.0, 0.0);    // 右上角
-    // points->InsertNextPoint(2.0, 1.0, 0.0);    // 右上凹点
-    // points->InsertNextPoint(2.0, 2.0, 0.0);    // 右中上
-    // points->InsertNextPoint(3.0, 2.0, 0.0);    // 右上角
-    // points->InsertNextPoint(3.0, 3.0, 0.0);    // 最右上角
-    // points->InsertNextPoint(0.0, 3.0, 0.0);    // 左上角
-    
-    // // 内凹部分 (顺时针方向)
-    // points->InsertNextPoint(0.0, 2.0, 0.0);    // 左中上
-    // points->InsertNextPoint(1.0, 2.0, 0.0);    // 中上
-    // points->InsertNextPoint(1.0, 1.0, 0.0);    // 中凹点
-    // points->InsertNextPoint(0.0, 1.0, 0.0);    // 左中下
-
 
     unsigned int gridSize = 10;
     for (size_t x = 0; x < gridSize; x++)
@@ -42,14 +28,43 @@ int main(int argc, char *argv[])
         }
     }
 
+
+    //顺时针扣洞，逆时针扣出来
+    std::vector<int> boundaryPts={0,1,2,3,4,5,6,7,17,27,37,47,46,45,35,25,24,23,22,21,20,10};
+
+    //下移一行，右一格
+    for (size_t i = 0; i < boundaryPts.size(); i++)
+    {
+        boundaryPts[i]+=11;
+    }
+
+    //反转
+    // std::reverse(boundaryPts.begin(),boundaryPts.end());
+
+
+
+    // 设置边界
+    vtkSmartPointer<vtkPolygon> poly = vtkSmartPointer<vtkPolygon>::New();
+    //必须要顺序连接，无法跨越，顺时针扣洞
+    for(auto i:boundaryPts)
+    {
+        poly->GetPointIds()->InsertNextId(i);
+    }
+    vtkSmartPointer<vtkCellArray> cells = vtkSmartPointer<vtkCellArray>::New();
+    cells->InsertNextCell(poly);
+
+    vtkSmartPointer<vtkPolyData> boundary = vtkSmartPointer<vtkPolyData>::New();
+    boundary->SetPoints(points);
+    boundary->SetPolys(cells);
+
     vtkSmartPointer<vtkPolyData> polydata =
         vtkSmartPointer<vtkPolyData>::New();
     polydata->SetPoints(points);
 
     vtkSmartPointer<vtkDelaunay2D> delaunay =
         vtkSmartPointer<vtkDelaunay2D>::New();
-
     delaunay->SetInputData(polydata);
+    delaunay->SetSourceData(boundary);
     delaunay->Update();
 
     vtkSmartPointer<vtkPolyDataMapper> mapper =

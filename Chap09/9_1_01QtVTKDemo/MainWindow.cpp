f22d3b1f515d7ba7e766a95498a420c296e3a18f
zhang Mr <185884274@qq.com>
2025-09-03 00:49:15 +0800

    添加qt按钮

--- Chap09/9_1_01QtVTKDemo/MainWindow.cpp
+++ Chap09/9_1_01QtVTKDemo/MainWindow.cpp
@@ -5,15 +5,21 @@
 #include <vtkActor.h>
 #include <vtkRenderer.h>
 #include <vtkGenericOpenGLRenderWindow.h>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
+
+
     QVTKOpenGLNativeWidget *vtkWidget = new QVTKOpenGLNativeWidget(this->ui->centralwidget);
     vtkWidget->setMinimumWidth(600);
 
-    ui->vtkContainer->addWidget(vtkWidget);
+    ui->vtkContainer->addWidget(vtkWidget, 0, 0, 2, 2);
+
+        // 添加工具条
+    ui->vtkContainer->addWidget(new QPushButton("Button"), 0, 0);
 
     // 2. (可选) 关联VTK渲染窗口
     //    也可以直接使用 vtkWidget->renderWindow()
@@ -27,21 +33,16 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     vtkNew<vtkActor> actor;
     actor->SetMapper(mapper);
 
-    double left[] = {0.0, 0.0, 0.5, 1.0};
-    double right[] = {0.5, 0.0, 1.0, 1.0};
-
-    vtkNew<vtkRenderer> leftRenderer;
-    leftRenderer->AddActor(actor);
-    leftRenderer->SetBackground(0.5, 0.5, 0.8); // 设置背景色
-    leftRenderer->SetViewport(left);
+    vtkNew<vtkRenderer> renderer;
+    renderer->AddActor(actor);
+    renderer->SetBackground(0.5, 0.5, 0.8); // 设置背景色
 
     vtkNew<vtkRenderer> rightRenderer;
     rightRenderer->AddActor(actor);
     rightRenderer->SetBackground(0.5, 0.5, 0); // 设置背景色
-    rightRenderer->SetViewport(right);
 
     // 4. 将渲染器添加到VTK渲染窗口
-    renderWindow->AddRenderer(leftRenderer);
+    renderWindow->AddRenderer(renderer);
     renderWindow->AddRenderer(rightRenderer);
     // 或者使用: renderWindow->AddRenderer(renderer);
 
